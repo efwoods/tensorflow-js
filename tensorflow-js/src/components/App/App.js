871356e7b3a2371a9771a6bdcf7059a1ca530ff4
Evan Woods <22337004+efwoods@users.noreply.github.com>
2023-04-19 11:08:03 -0400

    commit

--- tensorflow-js/src/components/App/App.js
+++ tensorflow-js/src/components/App/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const tf = require("@tensorflow/tfjs");
+
+  const model = tf.sequential();
+  model.add(
+    tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
+  );
+  model.add(tf.layers.dense({ units: 1, activation: "linear" }));
+  model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
+
+  const xs = tf.randomNormal([100, 10]);
+  const ys = tf.randomNormal([100, 1]);
+
+  // Train the model
+  model.fit(xs, ys, {
+    epochs: 100,
+    callbacks: {
+      onEpochEnd: (epoch, log) =>
+        console.log(`Epoch $(epoch): loss = ${log.loss}`),
+    },
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
